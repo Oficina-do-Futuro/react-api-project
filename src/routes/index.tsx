35a9a8eb78f8ddc30f895fdc26cfbbb19d035d31
uanela <uanelaluiswayne@gmail.com>
2026-06-13 12:20:46 +0200

    wip: add navbar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,29 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import type { ReactNode } from "react";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="bg-slate-800 p-4">
+        <nav>
+          <ul>
+            <NavItem to="/">Home</NavItem>
+            <NavItem to="/expenses/create">Create Expense</NavItem>
+            <NavItem to="/expense-categories/create">Create Category</NavItem>
+          </ul>
+        </nav>
+      </div>
+    </main>
+  );
+}
+function NavItem({ to, children }: { to: string; children: ReactNode }) {
+  return (
+    <li>
+      <Link to={to} className="hover:underline">
+        {children}
+      </Link>
+    </li>
+  );
 }
